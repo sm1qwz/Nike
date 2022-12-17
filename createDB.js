37c36d633fc4cc429cc9fd847ba12197facb8687
sm1qwz <mike_orion@mail.ru>
2022-12-17 18:47:24 +0300

    Создал БД

---
+++ createDB.js
@@ -0,0 +1,18 @@
+var MongoClient = require('mongodb').MongoClient
+
+const uri = "mongodb://127.0.0.1:27017/"
+const client = new MongoClient(uri)
+async function run() {
+   try {
+      await client.connect();
+      var database = client.db("nike");
+      database.dropDatabase()
+      database = client.db("nike");
+      const clothings = database.collection("clothing");
+      const result = await clothings.insertOne({ name: "Sneaker" });
+      console.log(`${result} documents were inserted`);
+   } finally {
+      await client.close();
+   }
+}
+run()

dcda3b074520957aeac228d7d49855d8e04faef1
sm1qwz <mike_orion@mail.ru>
2022-12-17 18:54:32 +0300

    В БД добавил данные

--- createDB.js
+++ createDB.js
@@ -5,15 +5,15 @@ const uri = "mongodb://127.0.0.1:27017/"
 const client = new MongoClient(uri)
 async function run() {
    try {
-      await client.connect();
-      var database = client.db("nike");
-      database.dropDatabase()
-      database = client.db("nike");
-      const clothings = database.collection("clothing");
-      const result = await clothings.insertOne({ name: "Sneaker" });
-      console.log(`${result} documents were inserted`);
-   } finally {
-      await client.close();
-   }
+    await client.connect();
+    var database = client.db("nike");
+    database.dropDatabase()
+    database = client.db("nike");
+    const clothings = database.collection("clothing");
+    const result = await clothings.insertMany(data);
+    console.log(`${result.insertedCount} documents were inserted`);
+ } finally {
+    await client.close();
+ }
 }
 run()

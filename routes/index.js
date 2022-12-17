b8bc10552f8c29023388727153140e564e0d843b
sm1qwz <mike_orion@mail.ru>
2022-12-17 18:05:00 +0300

    Добавил страницы

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* Страница кроссовок */
+router.get('/sneakers', function(req, res, next) {
+  res.send("<h1>Nike Shoes & Sneakers</h1>")
+});
+
+/* Страница футболок */
+router.get('/tshirts', function(req, res, next) {
+  res.send("<h1>Nike Tops & T-Shirts</h1>")
+});
+
+/* Страница футболок */
+router.get('/pants', function(req, res, next) {
+  res.send("<h1>Nike Pants & Tights</h1>")
+});
+
 module.exports = router;

42f1ab4a8ef0c25750516e7ece29f78e898ab06c
sm1qwz <mike_orion@mail.ru>
2022-12-17 19:06:18 +0300

    Создал схему

--- mongooseTasks.js
+++ mongooseTasks.js
@@ -1,13 +1,16 @@
+
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://127.0.0.1:27017/test')
 
-var Сlothing = mongoose.model('Сlothing', { name: String })
+var schema = mongoose.Schema({ name: String })
 
-var drill = new Сlothing({ name: 'Sneakers' })
+schema.methods.meow = function(){
+    console.log(this.get("name") + " are good for run ")
+}
+
+var Clothing = mongoose.model('Clothing', schema)
+
+var drill = new Clothing({ name: 'Sneakers' })
 drill.save(function (err) {
-    if (err) {
-        console.log(err)
-    } else {
-        console.log(' Nike shoes are lightweight and durable')
-    }
+    drill.meow()
 })

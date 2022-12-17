b98a47b9165d697fdd31a8bbf919864ac4794d58
sm1qwz <mike_orion@mail.ru>
2022-12-17 18:52:39 +0300

    Подготовил данные для бд

---
+++ data.js
@@ -0,0 +1,23 @@
+var data = [{
+    title: 'Nike Shoes & Sneakers',
+    nick: 'sneakers',
+    avatar: 'images/shoes.png',
+    desc: 'Nike shoes are lightweight and durable. Despite provides exceptional comfort, flexibility, of Nike shoes are very light. If you buy a pair of Nike shoes for specific sports, you will definitely reap the benefits for a long time.'
+ },
+ 
+ {
+    title: 'Nike Tops & T-Shirts',
+    nick: 'tshirts',
+    avatar: 'images/tshirt.jpg',
+    desc: 'A T-shirt (also spelled tee shirt), or tee, is a style of fabric shirt named after the T shape of its body and sleeves. Traditionally, it has short sleeves and a round neckline, known as a crew neck, which lacks a collar. T-shirts are generally made of a stretchy, light, and inexpensive fabric and are easy to clean.'
+ },
+ 
+ {
+    title: 'Nike Pants & Tights',
+    nick: 'pants',
+    avatar: 'images/pants.png',
+    desc: 'For warmth that doesn`t compromise breathability, choose trousers and tights from Nike. Whether you`re running or training, you`ll experience intense comfort in designs that let you step, bend and lunge towards your goals. Keep warm, stay focused'
+ }
+ ];
+ 
+ module.exports.data = data;
